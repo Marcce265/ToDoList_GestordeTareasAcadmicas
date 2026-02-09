dd8e58eb4f087ef9645f26993c071d1c204548b4
Ernesto <74765942@continental.edu.pe>
2026-02-09 01:47:26 -0500

    hola

--- src/tests/test_task_manager.py
+++
@@ -1,7 +0,0 @@
-
-# HU-001: Seleccionar perfil
-
-# 🔴 HU-001 CASO ROJO 1: seleccionar sin perfiles
-def test_hu001_rojo_sin_perfiles(self):
-    perfil = self.tm.seleccionar_perfil(1)
-    self.assertIsNone(perfil)
