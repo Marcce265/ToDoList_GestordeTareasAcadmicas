cd3808e1749fa29f73e69336f1ebd4213acd4198
Ernesto <74765942@continental.edu.pe>
2026-02-09 01:51:42 -0500

    test: HU-001 caso rojo seleccionar perfil sin perfiles

---
+++ src/tests/test_task_manager.py
@@ -0,0 +1,17 @@
+import unittest
+from src.logica.task_manager import TaskManager
+from src.modelo.modelo import Base, engine
+
+
+class TestTaskManager(unittest.TestCase):
+
+    def setUp(self):
+        Base.metadata.create_all(engine)
+        self.tm = TaskManager()
+
+    def tearDown(self):
+        Base.metadata.drop_all(engine)
+
+    def test_hu001_rojo_sin_perfiles(self):
+        perfil = self.tm.seleccionar_perfil(1)
+        self.assertIsNone(perfil)
