95c53a0529ae465a9e3601323354ea41307eeba7
Marcce265 <74765942@continental.edu.pe>
2026-02-09 20:31:33 -0500

    Conexión inicial establecida entre Reflex y carpeta src

--- src/modelo/declarative_base.py
+++ src/modelo/declarative_base.py
@@ -2,20 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Obtener la ruta del directorio actual (src/modelo)
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# Ruta absoluta a la base de datos
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+db_path = os.path.join(BASE_DIR, 'modelo', 'db.sqlite')
 
-# Crear la ruta de la base de datos en src/modelo
-db_path = os.path.join(current_dir, 'db.sqlite')
-# Configurar base de datos SQLite en el directorio src/modelo
 engine = create_engine(f'sqlite:///{db_path}', echo=False)
-# engine = create_engine(f'sqlite:///data.db', echo=True)
 Session = sessionmaker(bind=engine)
-
-Base = declarative_base()
-session = Session()
-
-# Crear la base de datos en memoria
-# engine = create_engine('sqlite:///:memory:')
-# Base.metadata.create_all(engine)
-# Session = sessionmaker(bind=engine)
+Base = declarative_base() # Esta es la Base que TODOS deben usar
